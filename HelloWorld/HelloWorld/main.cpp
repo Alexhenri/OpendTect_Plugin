0484d40c7b1fc0e7a9b3e97727bc9885a5e42236
Alexandre Henrique <alexhenrii13@gmail.com>
2019-04-30 11:25:01 -0300

    Added Hello World, and removed file that shouldn't be tracking

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Alexandre Henrique Silva on 29/04/19.
+//  Copyright © 2019 Alexhenri. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
